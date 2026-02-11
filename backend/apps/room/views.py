87b7b3c71239f293309575f16ea8de4dfe52ab83
rgt <rasbingodarthapa@gmail.com>
2026-02-11 16:56:18 +0545

    layout created for the project with basic necessary package installed

--- backend/apps/room/views.py
+++ backend/apps/room/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import (
+    RoomSerializer
+)
+from .models import (
+    Room, RoomImage
+)
+
+class RoomsListView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import { parse } from 'path'
 
 export default defineConfig([
   globalIgnores(['dist']),
